2ff2bbc44085cadfa60e4233ec7cdd8d4c2d4369
nwre87 <nwre87@gmail.com>
2018-01-24 19:36:35 -0800

    Add knex

---
+++ server/knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      database: 'vue_muesic',
+      charset: 'utf8'
+
+    },
+    migrations: {
+      directory: __dirname + './knex/migrations'
+    },
+    seeds: {
+      directory: __dirname + './knex/seeds'
+    }
+  },
+
+};
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+
 const PORT = 8081;
 
 const app = express();
@@ -9,7 +10,6 @@ const app = express();
 app.use(morgan('combine'));
 app.use(bodyParser.json());
 app.use(cors());
-
 
 app.use('/auth', require('./routes/auth'));
 
