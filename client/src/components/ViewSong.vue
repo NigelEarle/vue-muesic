d58553c48f958cbaa21baec72717788eecf2dd2f
nwre87 <nwre87@gmail.com>
2018-01-30 18:52:35 -0800

    Mark up and styles

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,11 +1,37 @@
 <template>
-  <panel title="Song Metadata">
-    {{song}}
-  </panel>
+  <v-layout>
+    <v-flex xs6>
+      <panel title="Song Metadata">
+        <v-layout>
+          <v-flex xs6>
+            <div class="song-title">
+              {{song.title}}
+            </div>
+            <div class="song-artist">
+              {{song.artist}}
+            </div>
+            <div class="song-genre">
+              {{song.genre}}
+            </div>
+          </v-flex>
+          <v-flex xs6>
+            <img :src="song.albumImageUrl" alt="" class="album-image">
+            {{song.album}}
+          </v-flex>
+        </v-layout>
+      </panel>
+    </v-flex>
+    <v-flex xs6 class="ml-2">
+      <panel title="tabs">
+        {{song.tab}}
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import SongsService from '@/services/songs';
 import Panel from '@/components/Panel';
+
 export default {
   data() {
     return {
@@ -24,5 +50,26 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
 
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
