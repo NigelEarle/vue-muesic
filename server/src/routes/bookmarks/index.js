e96ebf7dbefb9afe7f5cd38c8a324959b55ecc64
nwre87 <nwre87@gmail.com>
2018-02-02 20:43:09 -0800

    Fetch bookmark by songId

--- server/src/routes/bookmarks/index.js
+++ server/src/routes/bookmarks/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const knex = require('../../../knex/knex');
 const router = express.Router();
 
-router.route('/')
+router.route('/:songId')
 .get(async (req, res) => {
-  
+  const { songId } = req.params;
+  try {
+
+    const song = await knex('bookmarks').where('song_id', songId);
+    return res.status(200).send(song);
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+
 })
 
 module.exports = router
