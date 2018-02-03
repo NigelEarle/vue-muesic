9b8b9fec405d4ef8664c620c7355fb9c28823b22
nwre87 <nwre87@gmail.com>
2018-02-03 11:55:24 -0800

    lame

--- server/src/routes/bookmarks/index.js
+++ server/src/routes/bookmarks/index.js
@@ -12,14 +12,23 @@ router.route('/')
     // absolute trash - will refactor - tired from work
     if (!songId && userId) {
       bookmark = await knex('bookmarks').where('user_id', userId);
+      if (bookmark.length === 0) {
+        return res.status(200).send(null);
+      }
       return res.status(200).send(bookmark);
     }
 
     if (songId && !userId) {
-      bookmark = await knex('bookmarks').where('song_id', songId);
+      bookmark = await knex('bookmarks').where('song_id', songId).select('*');
+      if (bookmark.length === 0) {
+        return res.status(200).send(null);
+      }
       return res.status(200).send(bookmark);
     }
     bookmark = await knex('bookmarks').where('song_id', songId).andWhere('user_id', userId);
+    if (bookmark.length === 0) {
+      return res.status(200).send(null);
+    }
     return res.status(200).send(bookmark);
   } catch(err) {
     return res.status(500).send(err);
