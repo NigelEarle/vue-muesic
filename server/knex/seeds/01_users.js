a570e91e56404c8ed162aa3656c11790633ae7d4
nwre87 <nwre87@gmail.com>
2018-02-02 20:19:41 -0800

    Add users and songs seed files

--- server/knex/seeds/01_users.js
+++ server/knex/seeds/01_users.js
@@ -1,13 +1,50 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {
+          email: 'testing1@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing2@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing3@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing4@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing5@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing6@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing7@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing8@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing9@test.com',
+          password: '12345678'
+        },
+        {
+          email: 'testing0@test.com',
+          password: '12345678'
+        },
       ]);
     });
 };
