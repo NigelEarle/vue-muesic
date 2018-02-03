a72d81204341c4322b1a5c7a6ee8f6f47da19c84
nwre87 <nwre87@gmail.com>
2018-02-02 21:07:22 -0800

    Add shit conditional query on exist of query params

--- server/src/routes/bookmarks/index.js
+++ server/src/routes/bookmarks/index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const knex = require('../../../knex/knex');
 const router = express.Router();
 
-router.route('/:songId')
+router.route('/')
 .get(async (req, res) => {
-  const { songId } = req.params;
-  try {
+  const { songId, userId } = req.query;
 
-    const song = await knex('bookmarks').where('song_id', songId);
-    return res.status(200).send(song);
+  try {
+    let bookmark = null;
+
+    // absolute trash - will refactor - tired from work
+    if (!songId && userId) {
+      bookmark = await knex('bookmarks').where('user_id', userId);
+      return res.status(200).send(bookmark);
+    }
+
+    if (songId && !userId) {
+      bookmark = await knex('bookmarks').where('song_id', songId);
+      return res.status(200).send(bookmark);
+    }
+    console.log('HELLO')
+    bookmark = await knex('bookmarks').where('song_id', songId).andWhere('user_id', userId);
+    return res.status(200).send(bookmark);
   } catch(err) {
     return res.status(500).send(err);
   }
