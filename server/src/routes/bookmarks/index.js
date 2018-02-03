d7bed3aef1a44fc237362b92eeefd238b9280682
nwre87 <nwre87@gmail.com>
2018-02-02 20:36:18 -0800

    Add bookmarks route

---
+++ server/src/routes/bookmarks/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const knex = require('../../../knex/knex');
+const router = express.Router();
+
+router.route('/')
+.get(async (req, res) => {
+  
+})
+
+module.exports = router
