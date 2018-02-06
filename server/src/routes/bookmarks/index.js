8f40c9907a809082eeaaaf78edc0c7e8f4fc8f7a
nwre87 <nwre87@gmail.com>
2018-02-05 20:54:25 -0800

    remove fetch bookmarks conditions

--- server/src/routes/bookmarks/index.js
+++ server/src/routes/bookmarks/index.js
@@ -7,29 +7,13 @@ router.route('/')
   const { songId, userId } = req.query;
 
   try {
-    let bookmark = null;
-
-    // absolute trash - will refactor - tired from work
-    if (!songId && userId) {
-      bookmark = await knex('bookmarks').where('user_id', userId);
-      if (bookmark.length === 0) {
-        return res.status(200).send(null);
-      }
-      return res.status(200).send(bookmark);
-    }
-
-    if (songId && !userId) {
-      bookmark = await knex('bookmarks').where('song_id', songId).select('*');
-      if (bookmark.length === 0) {
-        return res.status(200).send(null);
-      }
-      return res.status(200).send(bookmark);
-    }
-    bookmark = await knex('bookmarks').where('song_id', songId).andWhere('user_id', userId);
+    const bookmark = await knex('bookmarks').where('song_id', songId).andWhere('user_id', userId);
+    
     if (bookmark.length === 0) {
       return res.status(200).send(null);
     }
     return res.status(200).send(bookmark);
+
   } catch(err) {
     return res.status(500).send(err);
   }
@@ -60,10 +44,9 @@ router.route('/')
 
 router.delete('/:bookmarkId', async (req, res) => {
   const { bookmarkId } = req.params;
-  console.log(bookmarkId)
+
   try {
     const data = await knex('bookmarks').where('id', bookmarkId).del();
-    console.log(data)
     return res.status(200).send('bookmark deleted');
   } catch (err) {
     return res.status(500).send(err);
