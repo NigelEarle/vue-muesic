de011309b130bf76dd6aa206cd42090d5db49592
nwre87 <nwre87@gmail.com>
2018-02-12 20:47:23 -0800

    Move passport to index

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const config = require('./config/config.json');
+const knex = require('../knex/knex');
+
 const morgan = require('morgan');
 const PORT = 8081;
 
 const app = express();
 
+passport.use(
+  new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwtSecret
+  }, async (jwtPayload, done) => {
+    try {
+      const user = await knex('users').where('id', jwtPayload.id);
+      if (!user) {
+        return done(new Error(), null);
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(new Error(), null);
+    }
+  })
+);
+
 app.use(morgan('combine'));
 app.use(bodyParser.json());
 app.use(cors());
 
-console.log(require('./passport'));
 
 app.use('/auth', require('./routes/auth'));
 app.use('/songs', require('./routes/songs'));
